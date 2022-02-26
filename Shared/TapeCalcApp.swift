be67a0b1219e24e9f3a06f8160063373eefd75c4
BPetty-InfoSec <bwpetty@protonmail.com>
2022-02-26 18:34:52 -0500

    Initial Commit

---
+++ Shared/TapeCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapeCalcApp.swift
+//  Shared
+//
+//  Created by Brian Petty on 2/26/22.
+//
+
+import SwiftUI
+
+@main
+struct TapeCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
